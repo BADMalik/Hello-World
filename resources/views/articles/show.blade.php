39297870cf83446505f960568b58a9663429f36f
Bilal Malik <malikzaman133@gmail.com>
2020-03-25 19:59:10 +0500

    3rd commit

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,6 +6,8 @@
             <div class="title">
                 <h2>{{$article->title}}</h2>
                  </div>
+                    <samp></samp>
+                  
             <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
             <p>{{$article->body}}</p>
         </div>
